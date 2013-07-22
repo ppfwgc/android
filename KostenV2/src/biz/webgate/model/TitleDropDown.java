32dfa6efd4e00e6573cad3757a50fe46c3b56e1b
Philipp Pfister <philipp.pfister@webgate.biz>
2013-07-22 15:56:40 +0200

    update dorpdown

---
+++ KostenV2/src/biz/webgate/model/TitleDropDown.java
@@ -0,0 +1,22 @@
+package biz.webgate.model;
+
+import java.util.ArrayList;
+
+public class TitleDropDown {
+
+	private final String m_Title = "TitleDropDown";
+	private ArrayList<String> m_TDL;
+	
+	public ArrayList<String> getTDL() {
+		return m_TDL;
+	}
+	public void setTDL(ArrayList<String> tDL) {
+		m_TDL = tDL;
+	}
+	public String getTitle() {
+		return m_Title;
+	}
+	
+	
+	
+}
